3d4ed62102ca294797e765cd6ce12e6466506ecf
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2025-01-10 01:48:19 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [socket, setSocket] = useState();
+  const inputRef = useRef();
+  function sendMessage() {
+    console.log("button clicked");
+    if (!socket) {
+      return;
+    }
+    const message = inputRef.current.value;
+    //@ts-ignore
+    socket.send(message);
+  }
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8000");
+    setSocket(ws);
+    ws.onmessage = (e) => {
+      alert(e.data);
+    };
+  }, []);
+  return (
+    <div>
+      chat app
+      <input type="text" placeholder="message" ref={inputRef} />
+      <button onClick={sendMessage}>send</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ websocket-scratch/src/index.ts
@@ -0,0 +1,11 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8000 });
+
+wss.on("connection", function (socket) {
+  console.log("user connected");
+  socket.on("message", (e) => {
+    if (e.toString() == "ping") {
+      socket.send("pong");
+    }
+  });
+});
